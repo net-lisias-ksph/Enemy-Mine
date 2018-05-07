3dbc2c0e11b9390d6b209e81b000c4d4a68b3013
DoctorDavinci <dr.dav@hotmail.com>
2018-05-07 16:09:14 -0400

    File Update - Initial Upload and In stall Package

--- EnemyMine_Plugin/Properties/AssemblyInfo.cs
+++ EnemyMine_Plugin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("OrX")]
-[assembly: AssemblyDescription("OrX")]
+[assembly: AssemblyTitle("Enemy Mine")]
+[assembly: AssemblyDescription("Enemy Mine")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("OrX")]
+[assembly: AssemblyProduct("Enemy Mine")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
